f6104c57eef4828c28b62287a9ac28d265fc96e4
Ben_Argaman <94112749+benalon98@users.noreply.github.com>
2022-09-16 05:44:43 +0300

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
-import express from "express";
 import * as yt from "youtube-search-without-api-key";
+import path from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 
 const app = express();
-app.listen(
-  process.env.PORT || 5002,
-  console.log("server running on port 5002")
-);
 
 app.get("/api/:title", async (req, res) => {
   const videos = await yt.search(req.params.title);
   res.json(videos[0].id.videoId);
 });
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
+app.listen(
+  process.env.PORT || 5002,
+  console.log("server running on port 5002")
+);
