54a0d90f61114e85a4783c6334de933793afcd49
Ben_Argaman <94112749+benalon98@users.noreply.github.com>
2022-09-16 05:15:06 +0300

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import * as yt from "youtube-search-without-api-key";
 
 const app = express();
-const portNumber = 5002;
-app.listen(portNumber || 5002, console.log("server running on port 5002"));
+app.listen(
+  process.env.PORT || 5002,
+  console.log("server running on port 5002")
+);
 
 app.get("/api/:title", async (req, res) => {
   const videos = await yt.search(req.params.title);
