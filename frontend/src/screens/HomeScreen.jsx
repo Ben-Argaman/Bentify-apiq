41065925a0d4989ad0cf48cb90c129726eea7df9
Ben_Argaman <94112749+benalon98@users.noreply.github.com>
2022-09-18 04:42:07 +0300

    player adjustments

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -147,7 +147,7 @@ const HomeScreen = () => {
         <div className="sticky bottom-0 ">
           <Player
             track={chosenTrack}
-            source={`http://localhost:5002/api/${query}`}
+            source={`https://ba-music.herokuapp.com/api/${query}`}
             albumImg={searchResults}
           />
         </div>
